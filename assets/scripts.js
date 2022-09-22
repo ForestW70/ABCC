febcc713aaeb28ce17ffa0cc87e862d83060e3a3
Forest wilson <hexaforest@gmail.com>
2022-09-22 19:06:15 -0400

    doc frags into view

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,14 +1,53 @@
-console.log("lets a go");
+import { disco } from "./directory.js"
+
+
+console.log(disco);
 
 (function(){
 
     const activeView = document.getElementById("pageView");
 
     const popGridView = () => {
-
-    }
-
-    const makeTile = () => {
         
+        disco.map((album, idx) => {
+            console.log(idx)
+            makeTile(album);
+        })
     }
+
+    const makeTile = (item) => {
+        // console.log(item.artlink);
+        let newView = document.createDocumentFragment();
+        const gridDiv = document.createElement("article");
+        gridDiv.classList.add("tile");
+
+        const tilePic = document.createElement("img");
+        // tilePic.src = `/goods/${item.artLink}`;
+        tilePic.src = item.webLink;
+        gridDiv.appendChild(tilePic);
+
+        const bigInfo = document.createElement("p");
+        bigInfo.classList.add("big-info");
+        bigInfo.innerText = `${item.artist} - ${item.title}`;
+        gridDiv.appendChild(bigInfo);
+
+
+
+        // console.log(item)
+        // item.map(infos => {
+        //     const titleRow = document.createElement("p");
+        //     titleRow.innerText = infos;
+        //     titleRow.classList.add(`tile-${infos}`);
+
+        //     gridDiv.appendChild(titleRow);
+
+        // })
+
+        newView.appendChild(gridDiv);
+        document.getElementById("pageView").appendChild(newView)
+
+    }
+
+
+    popGridView();
 })();
